dea6faf6471a3d5c0aa26e1b9e1294e042a92f44
olafnub <googlhoogle@gmail.com>
2023-04-19 00:06:21 -0500

    Add all files and first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Social media of casts from Love is Blind Season 4</h1>
+    </div>
+  );
+}
+
+export default App;
